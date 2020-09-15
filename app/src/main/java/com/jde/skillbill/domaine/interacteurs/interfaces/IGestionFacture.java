ea87eb1f7dbe1bd9ba375e3435928fe273092625
Julien Jacquard <jacquardjul@gmail.com>
2020-09-15 15:11:16 -0400

    Ajout des interfaces interacteurs, suppression de la classe Activite.java

--- app/src/main/java/com/jde/skillbill/domaine/interacteurs/interfaces/IGestionFacture.java
+++ app/src/main/java/com/jde/skillbill/domaine/interacteurs/interfaces/IGestionFacture.java
@@ -1,5 +1,6 @@
 package com.jde.skillbill.domaine.interacteurs.interfaces;
 
+import android.graphics.Bitmap;
 import com.jde.skillbill.domaine.entites.Facture;
 import com.jde.skillbill.domaine.entites.Paiement;
 import com.jde.skillbill.domaine.entites.Utilisateur;
@@ -10,5 +11,7 @@ public interface IGestionFacture {
     Facture creerFactureAvecMultiplePayeurs(double montantTotal, Map<Utilisateur, Double> mantantPayeParUtilisateur);
     Facture creerFacture(double montantTotal, Utilisateur utilisateurPayeur);
     Facture ajouterPaiement(Facture facture, Paiement paiement);
+    Facture ajouterPhotoFacture(Facture facture, String uri, Bitmap bitmap);
+    Facture modifierPhotoFacture(Facture facture, String uri, Bitmap bitmap);
 
 }
