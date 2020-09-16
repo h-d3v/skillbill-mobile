6c676dfb3308f3cb8a7a148de37f1cee1b5d5adf
Hedi <1756859@crosemont.qc.ca>
2020-09-16 01:21:32 -0400

    Ajout de la splash activity(version alpha), du logo, ansi que du ui de login, ui d'inscription a venir bientot!

---
+++ app/src/main/java/com/jde/skillbill/ui/activité/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.jde.skillbill.ui.activité;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jde.skillbill.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView logo, bgImg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        logo=findViewById(R.id.logo);
+        bgImg=findViewById(R.id.bgImg);
+        //animation de background et du logo pour creer un effet splash
+
+        bgImg.animate().translationY(-1600).setDuration(1500).setStartDelay(3500);
+        logo.animate().translationY(1400).setDuration(1500).setStartDelay(3500);
+
+    }
+}
