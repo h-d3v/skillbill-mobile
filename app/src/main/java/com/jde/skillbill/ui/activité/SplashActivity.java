777783944cfa567c1000ea1c00e88616f3756378
Hedi <1756859@crosemont.qc.ca>
2020-09-20 17:49:43 -0400

    delete activite generee par androidstudio

--- app/src/main/java/com/jde/skillbill/ui/activité/SplashActivity.java
+++ app/src/main/java/com/jde/skillbill/ui/activité/SplashActivity.java
@@ -11,16 +11,14 @@ public class SplashActivity extends AppCompatActivity {
 
     ImageView logo, bgImg;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    final protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
         logo=findViewById(R.id.logo);
         bgImg=findViewById(R.id.bgImg);
-        //animation de background et du logo pour creer un effet splash
 
+        //animation de background et du logo pour creer un effet splash
         bgImg.animate().translationY(-1600).setDuration(1500).setStartDelay(3500);
         logo.animate().translationY(1400).setDuration(1500).setStartDelay(3500);
-
     }
 }
