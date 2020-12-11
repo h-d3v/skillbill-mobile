6ac41373e4b9c6063eff719eb6eee18faae41a9e
Hedi <1756859@crosemont.qc.ca>
2020-12-11 12:05:00 -0500

    devises mises a jours avec des chiffres plus precis....

--- app/src/main/java/com/jde/skillbill/domaine/entites/Monnaie.java
+++ app/src/main/java/com/jde/skillbill/domaine/entites/Monnaie.java
@@ -2,10 +2,10 @@ package com.jde.skillbill.domaine.entites;
 
 public enum Monnaie {
     CAD(1, "CA$", 1),
-    USD(0.76, "US$", 1.31),
-    GBP(0.58, "£", 1.73),
-    EUR(0.64, "€",1.55),
-    AUD(1.05,"A$", 0.95);
+    USD(0.783038, "US$", 1.27708),
+    GBP(0.593066, "£", 1.68615),
+    EUR(0.646549, "€",1.54667) ,
+    AUD(1.03931,"A$", 0.962176);
     //reprsente la valeur du dollard canadien en la devise
     private final double tauxCad;
     private final String symbol;
